3370b502f1101c0d8c1221b57c3dda273f640848
Glauber Mendel <glaubermendel@gmail.com>
2026-03-26 17:26:48 -0300

    migrando conteudo para estrutura next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
 export default function Home() {
   return (
     <main>
-      <h1>Em construção</h1>
+      <h1>Glauber Mendel</h1>
+      <p>     Programador full stack apaixonado por desenvolvimento de
+          software e tecnologia. Tenho experiência com várias tecnologias web
+          modernas, incluindo HTML, CSS, JavaScript, React. </p>
+          
+<section>
+        <a href="/sobre" className="call-to-action">Saiba Mais</a>
+        <a href="/contato" className="call-to-action">Entre em Contato</a>
+      </section>
+
+      <section className="skills">
+        <h2>Habilidades</h2>
+        <ul>
+          <li>HTML, CSS e JavaScript</li>
+          <li>React e Next.js</li>
+          <li>Ruby on Rails</li>
+          <li>PostgreSQL, MongoDB e Redis</li>
+          <li>Versionamento de código com Git</li>
+          <li>Experiência em desenvolvimento ágil (Scrum)</li>
+        </ul>
+      </section>
+
     </main>
   )
 }
